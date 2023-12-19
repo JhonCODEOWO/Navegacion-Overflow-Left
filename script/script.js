b6675cb5106e669e9f5433dd1cc39c2f094d4352
JhonCode <jjv20618@gmail.com>
2023-12-19 15:16:38 -0600

    Se han cargado las operaciones al finalizar la carga del DOM

--- script/script.js
+++ script/script.js
@@ -1,12 +1,15 @@
-const scrollOverflow = document.querySelector("#overflow-takecontrol");
-const btnleft = document.querySelector("#btn-left");
-const btnright = document.querySelector("#btn-right");
 
-
-btnleft.addEventListener('click', function (event) {
-    scrollOverflow.scrollLeft -= 200;
-});
-
-btnright.addEventListener('click', function (event) {
-    scrollOverflow.scrollLeft += 200;
+document.addEventListener('DOMContentLoaded', function(){
+    const scrollOverflow = document.querySelector("#overflow-takecontrol");
+    const btnleft = document.querySelector("#btn-left");
+    const btnright = document.querySelector("#btn-right");
+    
+    
+    btnleft.addEventListener('click', function (event) {
+        scrollOverflow.scrollLeft -= 200;
+    });
+    
+    btnright.addEventListener('click', function (event) {
+        scrollOverflow.scrollLeft += 200;
+    });
 });
