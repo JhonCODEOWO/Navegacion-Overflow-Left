a9d90d77ae0cc86eea841f7ac476a6c09daad524
JhonCode <jjv20618@gmail.com>
2023-12-19 14:57:39 -0600

    Primera subida

---
+++ script/script.js
@@ -0,0 +1,12 @@
+const scrollOverflow = document.querySelector("#overflow-takecontrol");
+const btnleft = document.querySelector("#btn-left");
+const btnright = document.querySelector("#btn-right");
+
+
+btnleft.addEventListener('click', function (event) {
+    scrollOverflow.scrollLeft -= 200;
+});
+
+btnright.addEventListener('click', function (event) {
+    scrollOverflow.scrollLeft += 200;
+});
